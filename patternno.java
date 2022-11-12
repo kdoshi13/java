7feaaadee4b656deebb77eca72547012daff92cb
Keval Doshi <96608655+kdoshi13@users.noreply.github.com>
2022-11-12 19:10:44 +0530

    Update patternno.java

--- patternno.java
+++ patternno.java
@@ -1,22 +1,12 @@
-public class Pattern1  
-{  
-public static void main(String args[])   
-{   
-int i, j,number, n=5;   
-//loop for rows  
-for(i=0; i<n; i++)  
-{   
-number=5;   
-//loop for columns  
-for(j=0; j<=i; j++)  
-{   
-//prints num  
-System.out.print(number+ " ");   
-//incrementing the value of number   
-number--;   
-}   
-//throws the cursor at the next line after printing each row  
-System.out.println();   
-}   
-}   
-}  
+public class Pattern10 {
+	public static void main(String[] args) {
+		int n = 5;
+		for (int i = n ; i >= 1; i--) {
+			for (int j = i; j <= n; j++) {
+				System.out.print(j + " ");
+			}
+			System.out.println();
+		}
+		
+	}
+}
